cb115b0fab50c194b94aa763d537a37f337fe4fb
Shea Cronin <shealcronin@gmail.com>
2022-02-03 09:24:31 -0500

    Add index view and controller function to count data

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,7 +1,39 @@
 var Book = require('../models/book');
+var Author = require('../models/author');
+var Genre = require('../models/genre');
+var BookInstance = require('../models/bookinstance');
+
+var async = require('async');
 
 exports.index = function (req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    async.parallel(
+        {
+            book_count: (callback) => {
+                // Pass an empty object as match condition to find all documents
+                // of this collection
+                Book.countDocuments({}, callback);
+            },
+            book_instance_count: (callback) => {
+                BookInstance.countDocuments({}, callback);
+            },
+            book_instance_available_count: (callback) => {
+                BookInstance.countDocuments({ status: 'Available' }, callback);
+            },
+            author_count: (callback) => {
+                Author.countDocuments({}, callback);
+            },
+            genre_count: (callback) => {
+                Genre.countDocuments({}, callback);
+            },
+        },
+        (err, results) => {
+            res.render('index', {
+                title: 'Local Library Home',
+                error: err,
+                data: results,
+            });
+        }
+    );
 };
 
 // Display list of all books.
