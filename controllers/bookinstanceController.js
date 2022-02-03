b87557d5bd14e38e3656fc81c6ab0087541527bd
Shea Cronin <shealcronin@gmail.com>
2022-02-03 15:31:17 -0500

    Create add bookinstance form

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,6 @@
 var BookInstance = require('../models/bookinstance');
+var Book = require('../models/book');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function (req, res) {
@@ -38,13 +40,66 @@ exports.bookinstance_detail = function (req, res) {
 
 // Display BookInstance create form on GET.
 exports.bookinstance_create_get = function (req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+    Book.find({}, 'title').exec((err, books) => {
+        if (err) {
+            return next(err);
+        }
+        res.render('bookinstance_form', {
+            title: 'Create BookInstance',
+            book_list: books,
+        });
+    });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+    // Validate and sanitize fields.
+    body('book', 'Book must be specified').trim().isLength({ min: 1 }).escape(),
+    body('imprint', 'Imprint must be specified')
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body('status').escape(),
+    body('due_back', 'Invalid date')
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        var bookinstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back,
+        });
+
+        if (!errors.isEmpty()) {
+            Book.find({}, 'title').exec((err, books) => {
+                if (err) {
+                    return next(err);
+                }
+                res.render('bookinstance_form', {
+                    title: 'Create BookInstance',
+                    book_list: books,
+                    selected_book: bookinstance.book._id,
+                    errors: errors.array(),
+                    bookinstance: bookinstance,
+                });
+            });
+            return;
+        } else {
+            bookinstance.save((err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect(bookinstance.url);
+            });
+        }
+    },
+];
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function (req, res) {
