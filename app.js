d832dadc85f05b1c9da418d0c446233321c5157c
Shea Cronin <shealcronin@gmail.com>
2022-02-04 13:03:36 -0500

    Add files and changes required to deploy to Heroku

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
 // Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB =
+var dev_db_url =
     'mongodb+srv://shea:HDcEWSPv0iqauTLg@cluster0.ev70w.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -22,10 +25,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
